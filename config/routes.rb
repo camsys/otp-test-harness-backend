3024e9b9ca327f4977ee9065bc226bcfcf4d968b
Derek Edwards <dedwards8@gmail.com>
2017-10-13 17:11:42 -0400

    Create Trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   namespace :api do
+
+    ### API V1 ###
+    namespace :v1 do
+      resources :trips, only: [:index]
+    end
+  end
 end
