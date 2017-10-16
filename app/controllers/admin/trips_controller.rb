490b04ab35a754af47c9f3bc1c89fd5369eaf9f2
Derek Edwards <dedwards8@gmail.com>
2017-10-16 14:09:56 -0400

    Basic Trip Show Page

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -4,6 +4,10 @@ module Admin
     def index
       @trips = Trip.all 
     end
+
+     def show
+      @trip = Trip.find(params[:id])
+    end
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    
   namespace :admin do 
-    resources :trips, only: [:index]
+    resources :trips, only: [:index, :show]
   end
 
   namespace :api do
